91ac9c7bde61262efc18f0acec85ef494cd3896d
estebanmatias92 <estebanmatias92@gmail.com>
2022-12-05 11:56:34 -0300

    Added Service CustomEndpoint (Dependency Injection through Interface Argument)=

--- CustomEndpoint.cs
+++ CustomEndpoint.cs
@@ -5,9 +5,9 @@ namespace ASPCoreEmpty
     // New Service with depdency injection through HttpContext object
     public class CustomEndpoint
     {
-        public static async Task Endpoint(HttpContext context)
+        public static async Task Endpoint(HttpContext context, IResponseFormatter formatter)
         {
-            IResponseFormatter formatter = context.RequestServices.GetRequiredService<IResponseFormatter>();
+            //IResponseFormatter formatter = context.RequestServices.GetRequiredService<IResponseFormatter>();
 
             await formatter.Format(context, "Soy la clase Custom Endpoint");
         }
